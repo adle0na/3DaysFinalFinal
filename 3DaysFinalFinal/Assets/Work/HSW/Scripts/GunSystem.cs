7af3555c8f8b2bcdd456e3bf6d145b6eb3420192
Adle0na <uuuuufgh@gmail.com>
2022-04-15 18:15:48 +0900

    Remain Commit

---
+++ 3DaysFinalFinal/Assets/Work/HSW/Scripts/GunSystem.cs
@@ -0,0 +1,184 @@
+using TMPro;
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GunSystem : MonoBehaviour
+{
+    #region Gun Variables, Componenet, Sound, (SFX)
+    
+    // Gun stats
+    public int gunDamage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    private int bulletsLeft, bulletsShot;
+    public GameObject scopeOverlay;
+    public GameObject weaponCamera;
+    public Camera mainCamera;
+    public Rigidbody gunRb;
+
+    public float scopedFOV = 15f;
+    private float normalFOV;
+
+    private bool shooting, readyToShoot, reloading;
+    private bool isScoped = false;
+    
+    
+    // Reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    //public LayerMask whatIsEnemy;
+    public Animator animator;
+    
+    // Graphics ( Without Sound )
+    public CamShake camShake;
+    public float camShakeMagnitude, camShakeDuration;
+    public GameObject muzzleFlash, bulletHoleGraphic;
+    public TextMeshProUGUI text;
+    #endregion
+    
+    private void Awake()
+    {
+        gunRb = GetComponent<Rigidbody>();
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        GunInput();
+        
+        // SetText
+        text.SetText(bulletsLeft + " / " + magazineSize);
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isScoped = !isScoped;
+            animator.SetBool("Scoped", isScoped);
+
+            if (isScoped)
+            {
+                StartCoroutine(OnScoped());
+            }
+            else
+            {
+                OnUnscoped();
+            }
+                
+        }
+    }
+
+    #region Functions
+    
+    private void GunInput()
+    {
+        if (allowButtonHold)
+        {
+            shooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+        {
+            Reload();
+        }
+        
+        // Shoot
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+        
+        // Spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+        if (gunRb.velocity.magnitude > 0)
+        {
+            spread = spread * 1.5f;
+        }
+        else spread = spread;
+        
+        // Calculate Direction with Spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+        
+        // RayCast
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            if (rayHit.collider.CompareTag("Enemy") || rayHit.collider.CompareTag("Animal"))
+            {
+                Debug.Log("Enemy Or Animal Shot!");
+                // Null Reference ShootingAi, TakeDamage
+                //rayHit.collider.GetComponent<ShootingAi>().TakeDamage(gunDamage);
+            }
+            else
+            {
+                Debug.Log("What the hell");
+            }
+
+
+        }
+        
+        // ShakeCamera
+        camShake.Shake(camShakeMagnitude, camShakeDuration);
+        
+        // Graphics
+        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 100, 0));
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        
+        bulletsLeft--;
+        bulletsShot--;
+        
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if (bulletsShot > 0 && bulletsLeft > 0)
+        {
+            Invoke("Shoot", timeBetweenShooting);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+
+    void OnUnscoped()
+    {
+        scopeOverlay.SetActive(false);
+        weaponCamera.SetActive(true);
+
+        mainCamera.fieldOfView = normalFOV;
+    }
+
+    IEnumerator OnScoped()
+    {
+        yield return new WaitForSeconds(.15f);
+        
+        scopeOverlay.SetActive(true);
+        weaponCamera.SetActive(false);
+        normalFOV = mainCamera.fieldOfView;
+        mainCamera.fieldOfView = scopedFOV;
+    }
+    
+    #endregion
+}
